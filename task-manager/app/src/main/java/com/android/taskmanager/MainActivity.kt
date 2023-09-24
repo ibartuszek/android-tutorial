74ff6421646105a077fc9f4b4c2dfc9a4cb9a575
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-09-24 21:45:37 +0200

    u1: implement TaskCompleteItem

--- task-manager/app/src/main/java/com/android/taskmanager/MainActivity.kt
+++ task-manager/app/src/main/java/com/android/taskmanager/MainActivity.kt
@@ -3,13 +3,23 @@ package com.android.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.android.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             TaskManagerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    TaskCompleteItem()
                 }
             }
         }
@@ -27,17 +37,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskCompleteItem(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.task_completed_message),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                top = dimensionResource(R.dimen.tc_primary_top_padding),
+                bottom = dimensionResource(R.dimen.tc_primary_bottom_padding)
+            )
+        )
+        Text(
+            text = stringResource(R.string.task_completed_secondary_message),
+            fontSize = 16.sp, // TODO: extract
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskCompleteItemPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        TaskCompleteItem()
     }
 }

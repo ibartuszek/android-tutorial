e91fac8b3eb0c9a93ce78ce89ed68d9ada7f47e9
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-09-24 21:21:18 +0200

    u1: implement ComposeArticle

--- compose-article/app/src/main/java/com/android/composearticle/MainActivity.kt
+++ compose-article/app/src/main/java/com/android/composearticle/MainActivity.kt
@@ -3,13 +3,25 @@ package com.android.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.android.composearticle.model.ArticleData
+import com.android.composearticle.model.ArticleDataFactory
 import com.android.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +31,7 @@ class MainActivity : ComponentActivity() {
             ComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ComposeArticle(ArticleDataFactory.create())
                 }
             }
         }
@@ -27,17 +39,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeArticle(articleData: ArticleData, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(articleData.headerImageResource),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = stringResource(articleData.tile),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(all = dimensionResource(R.dimen.padding_medium))
+        )
+        Text(
+            text = stringResource(articleData.summary),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(
+                start = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium)
+            )
+        )
+        Text(
+            text = stringResource(articleData.content),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(all = dimensionResource(R.dimen.padding_medium))
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeArticlePreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ComposeArticle(ArticleDataFactory.create())
     }
 }

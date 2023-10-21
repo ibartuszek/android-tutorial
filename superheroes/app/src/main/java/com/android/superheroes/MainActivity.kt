086a6007da65e9238159c3de47c41739be55cb7e
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-10-21 19:00:00 +0200

    u3: add Scaffold with title

--- superheroes/app/src/main/java/com/android/superheroes/MainActivity.kt
+++ superheroes/app/src/main/java/com/android/superheroes/MainActivity.kt
@@ -3,13 +3,20 @@ package com.android.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.superheroes.model.HeroesRepository
 import com.android.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +26,47 @@ class MainActivity : ComponentActivity() {
             SuperheroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    HeroApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HeroApp() {
+    Scaffold(
+        topBar = {
+            HeroAppTopBar()
+        }
+    ) { contentPadding ->
+        HeroesScreen(heroes = HeroesRepository.heroes, contentPadding = contentPadding)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroAppTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+        },
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HeroAppPreview() {
     SuperheroesTheme {
-        Greeting("Android")
+        HeroApp()
     }
 }

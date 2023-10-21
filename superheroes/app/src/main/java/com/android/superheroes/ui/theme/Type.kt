b24317a691328ddefc062db264c0764bdc3f34ca
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2023-10-21 17:46:46 +0200

    u3: add fonts

--- superheroes/app/src/main/java/com/android/superheroes/ui/theme/Type.kt
+++ superheroes/app/src/main/java/com/android/superheroes/ui/theme/Type.kt
@@ -2,33 +2,51 @@ package com.android.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.android.superheroes.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_bold, FontWeight.Bold),
+    Font(R.font.cabin_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // Display is not needed on phones
+    // displayLarge = ...,
+    // displayMedium = ...,
+    // displaySmall = ...,
+    headlineLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    // titleLarge = ,
+    // titleMedium = ,
+    // titleSmall = ,
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    // bodyMedium = ,
+    // bodySmall = ,
+    // labelLarge = ,
+    // labelMedium = ,
+    // labelSmall = ,
 )
